a80f86856891c64a89c772bcf5f0514d87b028a0
AcademyGaffney <michaelkgaffney@gmail.com>
2020-10-20 09:56:00 -0500

    5

--- code/session5/geometry/grunner.py
+++ code/session5/geometry/grunner.py
@@ -1,5 +1,6 @@
 from point import Point
 
 p = Point(5, 3)
+q = Point(5, 3)
 
-print(p)
+print(p == q)
--- code/session5/geometry/point.py
+++ code/session5/geometry/point.py
@@ -1,3 +1,4 @@
+import math
 
 class Point:
 
@@ -6,13 +7,14 @@ class Point:
         self.y = y
 
     def setPoint(self, x, y):
-        pass
+        self.x = x
+        self.y = y
 
     def setX(self, x):
-        pass
+        self.x = x
 
     def setY(self, y):
-        pass
+        self.y = y
 
     def getX(self):
         return self.x
@@ -21,22 +23,35 @@ class Point:
         return self.y
 
     def movePoint(self, x, y):
-        pass
+        self.x += x
+        self.y += y
 
     def moveX(self, x):
-        pass
+        self.x += x
 
     def moveY(self, y):
-        pass
+        self.y += y
 
     def getDegrees(self):
-        return 0
+        degrees = math.atan2(self.x, self.y) / math.pi * 180
+        if degrees < 0:
+            return degrees + 360
+        return degrees
 
     def getDistance(self):
-        return 1
+        return (self.x ** 2 + self.y ** 2) ** .5
 
     def __str__(self):
         return "({},{})".format(self.x, self.y)
 
     def __eq__(self, other):
-        return True
+        if type(self) != type(other):
+            return False
+        xQuot = self.x / other.x
+        yQuot = self.y / other.y
+        if xQuot < 1:
+            xQuot = 1 / xQuotS
+        if yQuot < 1:
+            yQuot = 1 / yQuot
+        return xQuot < 1.000000000001 and yQuot < 1.000000000001
+
